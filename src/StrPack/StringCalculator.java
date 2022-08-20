09dd7db23f7583235db343804fd94a777391cc27
devMali <malidevendra40@gmail.com>
2022-08-20 16:20:07 +0530

    Return One num. argument & sum of two num seprated by comma

--- src/StrPack/StringCalculator.java
+++ src/StrPack/StringCalculator.java
@@ -4,6 +4,14 @@ public class StringCalculator {
 
 	public int add(String numbers)
 	{
-		return -1;
+		if(numbers.isEmpty())
+			return 0;
+		else if(numbers.length() ==1)
+			return Integer.parseInt(numbers);
+		else
+		{
+			String[] num  = numbers.split(",");
+			return Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+		}
 	}
 }
--- src/StrPack/StringCalculatorTest.java
+++ src/StrPack/StringCalculatorTest.java
@@ -7,9 +7,22 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	StringCalculator sc = new StringCalculator();
+	
 	@Test
 	public void testWithEmptyString() {
-		assertEquals(sc.add("0"),0);
+		assertEquals(sc.add(""),0);
 	}
+	
+	@Test
+	public void testWithOneNum() {
+		assertEquals(sc.add("1"),1);
+	}
+	
+	@Test
+	public void testWithTwoNum() {
+		assertEquals(sc.add("5,2"),7);
+	}
+	
+	
 
 }
